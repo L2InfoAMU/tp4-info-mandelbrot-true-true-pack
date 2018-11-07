49ebfdb25882cb00ced87a1c8dd5951fcd7d0b36
TP3 <ngguimsengryan@yahoo;fr>
2018-11-07 12:58:45 +0100

    Conjugate is fixed

--- src/main/java/mandelbrot/Complex.java
+++ src/main/java/mandelbrot/Complex.java
@@ -101,7 +101,7 @@ public class Complex {
      * @return A complex <code>c</code> such that <code>this * c = ||this|| ** 2</code>
      */
     Complex conjugate() {
-        return new Complex(-this.real, this.imaginary);
+        return new Complex(this.real, -this.imaginary);
     }
 
     /**
@@ -167,7 +167,7 @@ public class Complex {
      * @return the complex number <code>this / divisor</code>
      */
     Complex divide(Complex divisor) {
-        if (divisor.equals(I)){
+        if (divisor.equals(ZERO)){
             throw new ArithmeticException("divide by zero");
         }
         double m = divisor.squaredModulus();
